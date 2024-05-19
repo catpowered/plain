8464e09fdeb37d0d7255b267f682b11f2dea5b9a
Harry Bairstow <me@harryet.xyz>
2024-05-19 19:32:01 +0100

    feat: `upsertTenant` mutation

--- lib/plain.ex
+++ lib/plain.ex
@@ -2,11 +2,38 @@ defmodule Plain do
   defp base_url, do: "https://core-api.uk.plain.com/graphql/v1"
   defp token, do: Application.get_env(:plain, :token)
 
+  @createTenantMutation "mutations/upsertTenant.graphql" |> File.read!()
+  @spec createTenant(String.t(), String.t(), String.t() | nil) :: {:ok, map()} | {:error, any()}
+  def createTenant(identifier, name, url) do
+    send_request(@createTenantMutation, %{
+      "input" => %{
+        "identifier" => %{
+          "externalId" => identifier
+        },
+        "name" => name,
+        "externalId" => identifier,
+        "url" =>
+          if url != nil do
+            %{
+              "value" => url
+            }
+          else
+            nil
+          end
+      }
+    })
+  end
+
+  @spec createTenant(String.t(), String.t()) :: {:ok, map()} | {:error, any()}
+  def createTenant(identifier, name), do: createTenant(identifier, name, nil)
+
   @customerByIdQuery "queries/getCustomerById.graphql" |> File.read!()
+  @spec getCustomerById(String.t()) :: {:ok, map()} | {:error, any()}
   def getCustomerById(customer_id) do
     send_request(@customerByIdQuery, %{"customerId" => customer_id})
   end
 
+  @spec send_request(String.t(), map()) :: {:ok, map()} | {:error, any()}
   defp send_request(query, variables) do
     req =
       base_request()
